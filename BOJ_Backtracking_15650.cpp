a7d8819e2459479e8285a0710c16202c1ef0f779
JunHyeongKim73 <56298375+JunHyeongKim73@users.noreply.github.com>
2020-02-05 16:39:41 +0900

    Create BOJ_Backtracking_15650.cpp

---
+++ BOJ_Backtracking_15650.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int ary[9];
+bool visited[9];
+void solve(int n) {
+	if (n == M) {
+		for (int i = 0; i < M; i++)
+			printf("%d ", ary[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			if (n != 0)
+				if (ary[n - 1] >= i)
+					continue;
+			visited[i] = true;
+			ary[n] = i;
+			solve(n + 1);
+			//key point
+			visited[i] = false; 
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	solve(0);
+}
