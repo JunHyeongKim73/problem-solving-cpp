a21b710e5ffc64341240297ede23d8441f095fef
JunHyeongKim73 <56298375+JunHyeongKim73@users.noreply.github.com>
2020-02-05 16:48:03 +0900

    Create BOJ_Tracking_15652.cpp

---
+++ BOJ_Tracking_15652.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int ary[9];
+
+void solve(int n) {
+	if (n == M) {
+		for (int i = 0; i < M; i++)
+			printf("%d ", ary[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (n != 0)
+			if (ary[n - 1] > i)
+				continue;
+		ary[n] = i;
+		solve(n + 1);
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	solve(0);
+}
