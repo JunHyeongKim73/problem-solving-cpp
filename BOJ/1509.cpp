b6e5d4e8cee886ccf37d17519e8e46f432503c40
김준형 <rla5764v@naver.com>
2021-05-07 11:21:29 +0000

    Dynamic Programming & Palindrome

--- BOJ/1509.cpp
+++ BOJ/1509.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int cache[2501][2501];
 int dp[2501];
 string str;
-
+// 문자열의 start ~ (end - 1) Palindrome Check 
 int isPalindrome(int start, int end){
 	int& ret = cache[start][end];
 	if(ret != -1) return ret;
@@ -21,7 +21,7 @@ int isPalindrome(int start, int end){
 	}
 	return ret = 1;
 }
-
+// Palindrome의 집합으로 문자열을 표현하고 그 개수의 최솟값을 구한다.
 int solve(int start){
 	int len = str.length();
 	int& ret = dp[start];
